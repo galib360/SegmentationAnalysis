748c5308e34ed7f253f2a027df9d4980615910eb
galib360 <injamamul3.1416@gmail.com>
2019-03-28 12:36:00 +0100

    handled all non detection exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ float confThreshold = 0.5; // Confidence threshold
 float maskThreshold = 0.8; // Underestimation mask; more, the smaller the mask
 float maskThreshold2 = 0.15;// Overestimation mask; less, the bigger the mask
 int nFrames = 410;//Number of frames in a set
-int view = 16;//Number of sets
-int startView = 0;//Starting set, will run until it reaches the number of sets(View), Has to have same no. of frames
+int view = 8;//Number of sets
+int startView = 7;//Starting set, will run until it reaches the number of sets(View), Has to have same no. of frames
 int startFrame =0;
 int rectPad = 10;
 float R =0, P=0, F=0, A=0;//Final average evaluations
@@ -109,7 +109,7 @@ int main()
 						+ to_string(countFrame) + ".png";
 
 				pathGT = inputdir+"/Ground/cam" + to_string(countView) + "/"
-						+ to_string(countFrame) + ".pbm";
+						+ to_string(countFrame) + ".png";
 
 			}
 
@@ -165,7 +165,7 @@ int main()
 //			cout<<"ROWS: "<<silGT.rows<<", Cols : "<<silGT.cols<<endl;
 //			cout<<"ROWS: "<<silEst.rows<<", Cols : "<<silEst.cols<<endl;
 
-			float tn = 0, fp = 0, fn = 0, tp = 0;
+			float tn = 0, fp = 0.0000001, fn = 0.0000001, tp = 0;
 			float recall, precision, f1, accuracy;
 
 //			int countG =0;
@@ -222,7 +222,14 @@ int main()
 
 			recall = tp/(tp+fn);
 			precision = tp/(tp+fp);
-			f1 = 2 * ((precision*recall)/(precision+recall));
+			if(recall ==0 || precision ==0){
+				f1 = 0;
+				myfile<<"no detection in cam"<< countView<<" ,frame "<<countFrame<<endl;
+			}
+			else{
+				f1 = 2 * ((precision*recall)/(precision+recall));
+			}
+			//f1 = 2 * ((precision*recall)/(precision+recall));
 			accuracy = (tp+tn)/(tp+tn+fp+fn);
 
 //			cout<<"for set0"<<countView<<" ,frame "<<countFrame<<" ::::"<<endl;
@@ -316,6 +323,7 @@ Mat postprocess(Mat& frame, const vector<Mat>& outs, int& countFrame, int& count
     const int numClasses = outMasks.size[1];
 
     outDetections = outDetections.reshape(1, outDetections.total() / 7);
+//    cout<<"Number of detection: "<<numDetections<<endl;
     for (int i = 0; i < 1; ++i)//only taking one detection for all detection, i<numDetections
     {
         float score = outDetections.at<float>(i, 2);
@@ -341,154 +349,185 @@ Mat postprocess(Mat& frame, const vector<Mat>& outs, int& countFrame, int& count
 			resize(objectMask2, objectMask2, Size(box.width, box.height));
 			Mat mask = (objectMask > maskThreshold);
 			Mat mask2 = (objectMask2 > maskThreshold2);
+//			imshow("mask", objectMask);
+//			imshow("mask2", objectMask2);
+//			waitKey(0);
 			Mat fg = Mat::zeros(frame.rows, frame.cols, CV_8U);
 			Mat fg2 = Mat::zeros(frame.rows, frame.cols, CV_8U);
-			mask.copyTo(fg(box));
-			mask2.copyTo(fg2(box));
-			cv::threshold(fg, fg, 20, 255, cv::THRESH_BINARY);
-			cv::threshold(fg2, fg2, 20, 255, cv::THRESH_BINARY);
-			//mask.convertTo(mask, CV_8UC1);
-//			Mat coloredRoi = (0.3 * color + 0.7 * frame(box));
-//			coloredRoi.convertTo(coloredRoi, CV_8UC3);
+			try {
+				mask.copyTo(fg(box));
+				mask2.copyTo(fg2(box));
+				cv::threshold(fg, fg, 20, 255, cv::THRESH_BINARY);
+				cv::threshold(fg2, fg2, 20, 255, cv::THRESH_BINARY);
+				//mask.convertTo(mask, CV_8UC1);
+				//			Mat coloredRoi = (0.3 * color + 0.7 * frame(box));
+				//			coloredRoi.convertTo(coloredRoi, CV_8UC3);
 
-            // Draw bounding box, colorize and show the mask on the image
-            //drawBox(frame, classId, score, box, objectMask);
-//            imshow("fg", fg);
-//            imshow("fg2", fg2);
-//            waitKey(0);
+				// Draw bounding box, colorize and show the mask on the image
+				//drawBox(frame, classId, score, box, objectMask);
+				//            imshow("fg", fg);
+				//            imshow("fg2", fg2);
+				//            waitKey(0);
 
-            //Grabcut-------------------->
+				//Grabcut-------------------->
 
-            Mat1b markers(frame.rows, frame.cols);
-            // let's set all of them to possible background first
-            markers.setTo(cv::GC_PR_BGD);
+				Mat1b markers(frame.rows, frame.cols);
+				// let's set all of them to possible background first
+				markers.setTo(cv::GC_PR_BGD);
 
-            // cut out a small area in the middle of the image
-//            int m_rows = 0.1 * frame.rows;
-//            int m_cols = 0.1 * frame.cols;
-            int x = box.x;
-            int y = box.y;
-            int height = box.height;
-            int width = box.width;
+				// cut out a small area in the middle of the image
+				//            int m_rows = 0.1 * frame.rows;
+				//            int m_cols = 0.1 * frame.cols;
+				int x = box.x;
+				int y = box.y;
+				int height = box.height;
+				int width = box.width;
 
-            // of course here you could also use cv::Rect() instead of cv::Range to select
-            // the region of interest
-//            cv::Mat1b fg_seed = markers(Range(((x+width/2) - 10), ((x+width/2) + 10)),
-//            							Range(((y+height/2) - 10), ((y+height/2) + 10)));
+				// of course here you could also use cv::Rect() instead of cv::Range to select
+				// the region of interest
+				//            cv::Mat1b fg_seed = markers(Range(((x+width/2) - 10), ((x+width/2) + 10)),
+				//            							Range(((y+height/2) - 10), ((y+height/2) + 10)));
 
-            //Mat1b fg_seed = markers(Rect(((x+width)/2)-5, ((y+height)/2)-5, ((x+width)/2)+5, ((y+height)/2)+5));
-            Mat1b fg_seed = markers(Rect(((x+width)/2)-5, ((y+height)/2)-5, 10, 10));
-            Mat1b fg_rect = markers(Rect(left, top, right - left + 1, bottom - top + 1));
-//            Mat1b fg_seed2 = markers(cv::Range(frame.rows/2 - m_rows/2, frame.rows/2 + m_rows/2),
-//            		                 cv::Range(frame.cols/2 - m_cols/2, frame.cols/2 + m_cols/2));
-//            Mat1b fg_vert = markers(Rect(((x+width)/2)-2, y+5, 20, height));
-//            cv::Mat1b fg_seed = markers(cv::Range(frame.rows/2 - m_rows/2, frame.rows/2 + m_rows/2),
-//                                        cv::Range(frame.cols/2 - m_cols/2, frame.cols/2 + m_cols/2));
-            //cv::Mat1b fg_seed = markers(Rect(x, y, x+width, y+height));
-            // mark it as foreground
+				//Mat1b fg_seed = markers(Rect(((x+width)/2)-5, ((y+height)/2)-5, ((x+width)/2)+5, ((y+height)/2)+5));
+				Mat1b fg_seed = markers(Rect(((x+width)/2)-5, ((y+height)/2)-5, 10, 10));
+				Mat1b fg_rect = markers(Rect(left, top, right - left + 1, bottom - top + 1));
+				//            Mat1b fg_seed2 = markers(cv::Range(frame.rows/2 - m_rows/2, frame.rows/2 + m_rows/2),
+				//            		                 cv::Range(frame.cols/2 - m_cols/2, frame.cols/2 + m_cols/2));
+				//            Mat1b fg_vert = markers(Rect(((x+width)/2)-2, y+5, 20, height));
+				//            cv::Mat1b fg_seed = markers(cv::Range(frame.rows/2 - m_rows/2, frame.rows/2 + m_rows/2),
+				//                                        cv::Range(frame.cols/2 - m_cols/2, frame.cols/2 + m_cols/2));
+				//cv::Mat1b fg_seed = markers(Rect(x, y, x+width, y+height));
+				// mark it as foreground
 
-            //fg_rect.setTo(GC_PR_FGD);
-            //fg_seed2.setTo(GC_FGD);
-            Mat1b fgp;
-//            cout<<"width: "<<width<<" , height: "<<height<<" ,x: "<<x<<" ,y: "<<y<<endl;
-//            cout<<"left "<<((x+width)/2)-5<<" , top: "<<((y+height)/2)-5<<" ,right: "<<((x+width)/2)+5<<" ,bottom: "<<((y+height)/2)+5<<endl;
+				//fg_rect.setTo(GC_PR_FGD);
+				//fg_seed2.setTo(GC_FGD);
+				Mat1b fgp;
+				//            cout<<"width: "<<width<<" , height: "<<height<<" ,x: "<<x<<" ,y: "<<y<<endl;
+				//            cout<<"left "<<((x+width)/2)-5<<" , top: "<<((y+height)/2)-5<<" ,right: "<<((x+width)/2)+5<<" ,bottom: "<<((y+height)/2)+5<<endl;
 
 
 
-            //Mat curr_sil_bin(fg.rows, fg.cols, CV_32F);
-			//int count = 0;
-			for (int row = y; row < y+height; row++) {
-				for (int col = x; col < x+width; col++) {
-					if (fg2.at<uchar>(row, col) != 0) {
-						if (fg.at<uchar>(row, col) != 0) {
-							//curr_sil_bin.at<float>(row, col) = 1;
-							//count++;
-							markers.at<uchar>(row, col) = GC_FGD;
+				//Mat curr_sil_bin(fg.rows, fg.cols, CV_32F);
+				//int count = 0;
+				for (int row = y; row < y+height; row++) {
+					for (int col = x; col < x+width; col++) {
+						if (fg2.at<uchar>(row, col) != 0) {
+							if (fg.at<uchar>(row, col) != 0) {
+								//curr_sil_bin.at<float>(row, col) = 1;
+								//count++;
+								markers.at<uchar>(row, col) = GC_FGD;
 
+							}
+							else{
+								markers.at<uchar>(row, col) = GC_PR_FGD;
+							}
 						}
 						else{
-							markers.at<uchar>(row, col) = GC_PR_FGD;
+							markers.at<uchar>(row, col) = GC_BGD;
 						}
-					}
-					else{
-						markers.at<uchar>(row, col) = GC_BGD;
-					}
 
-//					if(fg.at<uchar>(row, col) == 0){
-//						//curr_sil_bin.at<float>(row, col) = 0;
-//						markers.at<uchar>(row, col) = GC_PR_FGD;
-//					}
-//					if (fg2.at<uchar>(row, col) == 0) {
-//						//curr_sil_bin.at<float>(row, col) = 0;
-//						markers.at<uchar>(row, col) = GC_BGD;
-//					}
+				//					if(fg.at<uchar>(row, col) == 0){
+				//						//curr_sil_bin.at<float>(row, col) = 0;
+				//						markers.at<uchar>(row, col) = GC_PR_FGD;
+				//					}
+				//					if (fg2.at<uchar>(row, col) == 0) {
+				//						//curr_sil_bin.at<float>(row, col) = 0;
+				//						markers.at<uchar>(row, col) = GC_BGD;
+				//					}
+					}
 				}
+
+				//fg_seed.setTo(GC_FGD);
+				//fg_vert.setTo(GC_FGD);
+				//fg_seed=GC_FGD;
+				//fg_vert=GC_FGD;
+
+
+				// select pixels that lie outside the bounding rect
+				//cv::Mat1b bg_seed = markers(cv::Range(0, 5),cv::Range::all());
+				Mat1b bg_seed1 = markers(cv::Range::all(),cv::Range(0, x));
+				Mat1b bg_seed2 = markers(cv::Range::all(),cv::Range(x+width, frame.cols));
+				Mat1b bg_seed3 = markers(cv::Range(y+height, frame.rows),cv::Range::all());
+				Mat1b bg_seed4 = markers(cv::Range(0, y),cv::Range::all());
+
+				//            cv::Mat1b bg_seed5 = markers(cv::Range(y+height-20, y+height),cv::Range(x, x+20));
+				//            cv::Mat1b bg_seed6 = markers(cv::Range(y+height-5, y+height),cv::Range((x+width)/2, x+width));
+				//            Mat1b bg_seed;
+				//            bg_seed1.copyTo(bg_seed);
+				//            bg_seed2.copyTo(bg_seed);
+
+				//Setting surely background pixels
+				//bg_seed.setTo(cv::GC_BGD);
+				bg_seed1.setTo(GC_BGD);
+				bg_seed2.setTo(GC_BGD);
+				bg_seed3.setTo(GC_BGD);
+				bg_seed4.setTo(GC_BGD);
+				//bg_seed5.setTo(GC_BGD);
+				//bg_seed6.setTo(GC_BGD);
+
+				//with Mask
+
+				Mat bgd, fgd;
+				int iterations = 1;
+				cv::grabCut(frame, markers, Rect(), bgd, fgd, iterations, cv::GC_EVAL);
+				//cv::grabCut(frame, markers, Rect(), bgd, fgd, iterations, cv::GC_INIT_WITH_MASK);
+				// let's get all foreground and possible foreground pixels
+				cv::Mat1b mask_fgpf = ( markers == cv::GC_FGD) | ( markers == cv::GC_PR_FGD);
+				// and copy all the foreground-pixels to a temporary image
+				cv::Mat3b tmp = cv::Mat3b::zeros(frame.rows, frame.cols);
+				//            imshow("temp", mask_fgpf);
+				//            waitKey(0);
+				frame.copyTo(tmp, mask_fgpf);
+
+				//smoothen
+				for ( int i = 1; i < 7; i = i + 2 ){
+					medianBlur ( mask_fgpf, mask_fgpf, i );
+				}
+
+				// show it
+				//            cv::imshow("foreground", tmp);
+				//            cv::waitKey(0);
+
+				string outputPath;
+				if(countView<10){
+					outputPath = inputdir+"/Est/cam0" + to_string(countView)+ "/"+to_string(countFrame)+".png";
+				}
+				else if(countView>=10){
+					outputPath = inputdir+"/Est/cam" + to_string(countView)+ "/"+to_string(countFrame)+".png";
+				}
+
+				//            string outputPath = "dancer4D/Est/cam0" + to_string(countView)+ "/"+to_string(countFrame)+".png";
+				//            string outputPath = "experiment/cam0" + to_string(countView) + "/silscam0" + to_string(countView)+ "/"+to_string(countFrame)+".jpg";
+				imwrite(outputPath, mask_fgpf);
+				return mask_fgpf;
+			} catch (Exception &e) {
+				myfile<<"no detection in cam"<< countView<<" ,frame "<<countFrame<<endl;
+				Mat mask_fgpf(frame.size(), CV_8UC1, Scalar(0));
+				string outputPath;
+				if (countView < 10) {
+					outputPath = inputdir + "/Est/cam0" + to_string(countView)
+							+ "/" + to_string(countFrame) + ".png";
+				} else if (countView >= 10) {
+					outputPath = inputdir + "/Est/cam" + to_string(countView)
+							+ "/" + to_string(countFrame) + ".png";
+				}
+				imwrite(outputPath, mask_fgpf);
+				return mask_fgpf;
 			}
 
-			//fg_seed.setTo(GC_FGD);
-			//fg_vert.setTo(GC_FGD);
-			//fg_seed=GC_FGD;
-			//fg_vert=GC_FGD;
-
-
-            // select pixels that lie outside the bounding rect
-            //cv::Mat1b bg_seed = markers(cv::Range(0, 5),cv::Range::all());
-            Mat1b bg_seed1 = markers(cv::Range::all(),cv::Range(0, x));
-            Mat1b bg_seed2 = markers(cv::Range::all(),cv::Range(x+width, frame.cols));
-            Mat1b bg_seed3 = markers(cv::Range(y+height, frame.rows),cv::Range::all());
-            Mat1b bg_seed4 = markers(cv::Range(0, y),cv::Range::all());
-
-//            cv::Mat1b bg_seed5 = markers(cv::Range(y+height-20, y+height),cv::Range(x, x+20));
-//            cv::Mat1b bg_seed6 = markers(cv::Range(y+height-5, y+height),cv::Range((x+width)/2, x+width));
-//            Mat1b bg_seed;
-//            bg_seed1.copyTo(bg_seed);
-//            bg_seed2.copyTo(bg_seed);
-
-            //Setting surely background pixels
-            //bg_seed.setTo(cv::GC_BGD);
-            bg_seed1.setTo(GC_BGD);
-            bg_seed2.setTo(GC_BGD);
-            bg_seed3.setTo(GC_BGD);
-            bg_seed4.setTo(GC_BGD);
-            //bg_seed5.setTo(GC_BGD);
-            //bg_seed6.setTo(GC_BGD);
-
-            //with Mask
-
-            Mat bgd, fgd;
-            int iterations = 1;
-            cv::grabCut(frame, markers, Rect(), bgd, fgd, iterations, cv::GC_EVAL);
-            //cv::grabCut(frame, markers, Rect(), bgd, fgd, iterations, cv::GC_INIT_WITH_MASK);
-            // let's get all foreground and possible foreground pixels
-            cv::Mat1b mask_fgpf = ( markers == cv::GC_FGD) | ( markers == cv::GC_PR_FGD);
-            // and copy all the foreground-pixels to a temporary image
-            cv::Mat3b tmp = cv::Mat3b::zeros(frame.rows, frame.cols);
-//            imshow("temp", mask_fgpf);
-//            waitKey(0);
-            frame.copyTo(tmp, mask_fgpf);
-
-            //smoothen
-            for ( int i = 1; i < 7; i = i + 2 ){
-            	medianBlur ( mask_fgpf, mask_fgpf, i );
-            }
-
-            // show it
-//            cv::imshow("foreground", tmp);
-//            cv::waitKey(0);
-
-            string outputPath;
-            if(countView<10){
-            	outputPath = inputdir+"/Est/cam0" + to_string(countView)+ "/"+to_string(countFrame)+".png";
-            }
-            else if(countView>=10){
-            	outputPath = inputdir+"/Est/cam" + to_string(countView)+ "/"+to_string(countFrame)+".png";
-            }
-
-//            string outputPath = "dancer4D/Est/cam0" + to_string(countView)+ "/"+to_string(countFrame)+".png";
-//            string outputPath = "experiment/cam0" + to_string(countView) + "/silscam0" + to_string(countView)+ "/"+to_string(countFrame)+".jpg";
-            imwrite(outputPath, mask_fgpf);
-            return mask_fgpf;
-
+        }
+        else{
+        	Mat mask_fgpf(frame.size(), CV_8UC1, Scalar(0));
+			string outputPath;
+			if (countView < 10) {
+				outputPath = inputdir + "/Est/cam0" + to_string(countView) + "/"
+						+ to_string(countFrame) + ".png";
+			} else if (countView >= 10) {
+				outputPath = inputdir + "/Est/cam" + to_string(countView) + "/"
+						+ to_string(countFrame) + ".png";
+			}
+        	imwrite(outputPath, mask_fgpf);
+        	return mask_fgpf;
         }
     }
 }
